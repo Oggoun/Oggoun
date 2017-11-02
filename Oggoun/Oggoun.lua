ff9eb6ba406ab759ad446220e1ef062d63ba3715
Oggoun <oggaun+github@gmail.com>
2017-11-02 18:22:21 +0100

    Initial commit

---
+++ Oggoun/Oggoun.lua
@@ -0,0 +1,20 @@
+local LSM = LibStub("LibSharedMedia-3.0")
+
+LSM:Register("font", "Roboto Regular",		[[Interface\AddOns\Odin\Fonts\Roboto-Regular.ttf]])
+LSM:Register("font", "Roboto Medium",		[[Interface\AddOns\Odin\Fonts\Roboto-Medium.ttf]])
+LSM:Register("font", "Roboto Bold",			[[Interface\AddOns\Odin\Fonts\Roboto-Bold.ttf]])
+LSM:Register("font", "Roboto Black ",		[[Interface\AddOns\Odin\Fonts\Roboto-Black.ttf]])
+
+LSM:Register("font", "RobotoC Regular",		[[Interface\AddOns\Odin\Fonts\RobotoCondensed-Regular.ttf]])
+LSM:Register("font", "RobotoC Bold",		[[Interface\AddOns\Odin\Fonts\RobotoCondensed-Bold.ttf]])
+
+LSM:Register("font", "SFT Regular",			[[Interface\AddOns\Odin\Fonts\SanFranciscoText-Regular.ttf]])
+LSM:Register("font", "SFT Light",			[[Interface\AddOns\Odin\Fonts\SanFranciscoText-Light.ttf]])
+LSM:Register("font", "SFT Medium",			[[Interface\AddOns\Odin\Fonts\SanFranciscoText-Medium.ttf]])
+LSM:Register("font", "SFT SemiBold",		[[Interface\AddOns\Odin\Fonts\SanFranciscoText-SemiBold.ttf]])
+
+LSM:Register("font", "PT Sans Narrow",		[[Interface\AddOns\Odin\Fonts\PTSans-Narrow.ttf]])
+
+LSM:Register("sound", "Roar",				[[Interface\AddOns\Odin\Sounds\roar.ogg]])
+LSM:Register("sound", "Electro High",		[[Interface\AddOns\Odin\Sounds\electro_high.ogg]])
+LSM:Register("sound", "Electro Low",		[[Interface\AddOns\Odin\Sounds\electro_low.ogg]])
